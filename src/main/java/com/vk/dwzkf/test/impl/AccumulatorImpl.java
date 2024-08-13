6fcb1cdabd8a7d2b84a2e062fbbd590289bd8b7a
pemponce <a.zheltuhin@mail.ru>
2024-08-13 23:40:38 +0300

    9 of 9 tests

--- src/main/java/com/vk/dwzkf/test/impl/AccumulatorImpl.java
+++ src/main/java/com/vk/dwzkf/test/impl/AccumulatorImpl.java
@@ -12,16 +12,31 @@ import java.util.*;
 public class AccumulatorImpl implements Accumulator {
     private final Map<Long, PriorityQueue<StateObject>> processMap = new HashMap<>();
     private State lastState = null;
+    private int count = 0;
 
     @Override
     public void accept(StateObject stateObject) {
         Long processId = stateObject.getProcessId();
         PriorityQueue<StateObject> stateObjects = processMap.computeIfAbsent(processId, k ->
-                new PriorityQueue<>(Comparator.comparing(StateObject::getState)));
+                new PriorityQueue<>(new StateComparator()));
 
         if (!stateObjects.isEmpty() && isFinalState(stateObjects.peek().getState())) return;
 
         stateObjects.add(stateObject);
+    }
+
+    private static class StateComparator implements Comparator<StateObject> {
+        @Override
+        public int compare(StateObject o1, StateObject o2) {
+            State state1 = o1.getState();
+            State state2 = o2.getState();
+
+            if ((state1 == State.MID1 && state2 == State.MID2) || (state1 == State.MID2 && state2 == State.MID1)) {
+                return 3;
+            }
+
+            return state1.compareTo(state2);
+        }
     }
 
     private boolean isFinalState(State state) {
@@ -36,22 +51,25 @@ public class AccumulatorImpl implements Accumulator {
     @Override
     public List<StateObject> drain(Long processId) {
         PriorityQueue<StateObject> stateObjects = processMap.get(processId);
+        List<StateObject> list = new ArrayList<>(stateObjects.stream().toList());
+        for(int i = 0; i < count; i++) {
+            stateObjects.remove(list.get(i));
+        }
+        count = 0;
         List<StateObject> res = new ArrayList<>();
-
-        if (stateObjects == null) return Collections.emptyList();
 
         for (StateObject stateObject : stateObjects) {
             if (stateObjects.size() == 1 && lastState == null) {
                 if (stateObject.getState() == State.START2) {
                     lastState = stateObject.getState();
                     res.add(stateObject);
-                    stateObjects.remove(stateObject);
                 }
             } else if (lastState != null && isFinalState(lastState)) break;
             else if (lastState == null || isValidTransition(lastState, stateObject.getState())) {
                 res.add(stateObject);
                 lastState = stateObject.getState();
             }
+            count++;
         }
         return res;
     }
@@ -67,7 +85,7 @@ public class AccumulatorImpl implements Accumulator {
                     currentState == State.START1 || currentState == State.MID1 || (currentState == State.FINAL1 ||
                             currentState == State.FINAL2) && isFinalState(currentState);
             case MID1 ->
-                    currentState == State.MID2 || (currentState == State.FINAL1 || currentState == State.FINAL2) &&
+                    currentState == State.MID1 || currentState == State.MID2 || (currentState == State.FINAL1 || currentState == State.FINAL2) &&
                             isFinalState(currentState);
             case FINAL2 ->
                     isFinalState(currentState) || currentState == State.START1;
